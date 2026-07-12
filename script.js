0cc6e7100f8095ca8af74e6e76d2575cce706146
sabzyhacks <ei711283@gmail.com>
2026-07-12 23:49:05 +0100

    Added invalid number verification and updated missing input verification

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ btn.addEventListener("click", function (e) {
   const height = Number(heightInp.value);
   const weight = Number(weightInp.value);
 
-  if (height <= 0 || weight <= 0) {
+  // Missing inputs
+  if (heightInp.value === "" || weightInp.value === "") {
     err.innerHTML = "Fields must not be empty!";
     err.classList.add("active");
+    // Invalid number
+  } else if (height <= 0 || weight <= 0) {
+    err.innerHTML = "Not a valid weight or height!";
+    err.classList.add("active");
+  } else {
+    err.classList.remove("active");
   }
 });
-
-// Missing inputs
