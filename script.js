991476cb25302236902c693cb19e8a7dcde92c76
sabzyhacks <ei711283@gmail.com>
2026-07-13 00:12:25 +0100

    BMI calculation in place

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const weightInp = document.querySelector("#weight");
 const btn = document.querySelector("#submit");
 const err = document.querySelector(".error");
 
+const calcBmi = function (height, weight) {
+  return parseFloat((weight / (height / 100) ** 2).toFixed(1));
+};
+
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   const height = Number(heightInp.value);
@@ -18,6 +22,7 @@ btn.addEventListener("click", function (e) {
     err.innerHTML = "Not a valid weight or height!";
     err.classList.add("active");
   } else {
+    console.log(calcBmi(height, weight));
     err.classList.remove("active");
   }
 });
