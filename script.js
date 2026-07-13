6d91ff1f811688fb1064d225a5bde5f14ca555b9
sabzyhacks <ei711283@gmail.com>
2026-07-13 13:58:51 +0100

    Added display bmi and category, also refactored code

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 "use strict";
+const output = document.querySelector(".output");
+const dispBmi = document.querySelector(".bmi > h2");
+const dispDesc = document.querySelector(".bmi > p");
 const heightInp = document.querySelector("#height");
 const weightInp = document.querySelector("#weight");
 const btn = document.querySelector("#submit");
 const err = document.querySelector(".error");
 
-let bmi = 0;
-let bmiRange = "";
-
 const calcBmi = function (height, weight) {
   return parseFloat((weight / (height / 100) ** 2).toFixed(1));
+};
+
+const getBmiDesc = function (bmi) {
+  return `${bmi >= 30.0 ? "You're Obese" : bmi >= 25.0 && bmi <= 29.9 ? "You're Overweight" : bmi >= 18.5 && bmi <= 24.9 ? "You have normal weight" : "You're Underweight"}`;
 };
 
 btn.addEventListener("click", function (e) {
@@ -25,11 +29,12 @@ btn.addEventListener("click", function (e) {
     err.innerHTML = "Not a valid weight or height!";
     err.classList.add("active");
   } else {
-    bmi = calcBmi(height, weight);
-    bmiRange = `${bmi >= 30.0 ? "You're Obese" : bmi >= 25.0 && bmi <= 29.9 ? "You're Overweight" : bmi >= 18.5 && bmi <= 24.9 ? "You have normal weight" : "You're Underweight"}`;
-
-    console.log(bmi);
-    console.log(bmiRange);
+    // Remove active class from error
     err.classList.remove("active");
+
+    // Display
+    output.style.display = "flex";
+    dispBmi.innerHTML = calcBmi(height, weight);
+    dispDesc.innerHTML = getBmiDesc(bmi);
   }
 });
